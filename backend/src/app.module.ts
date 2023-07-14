500e8503a79d13dfded4eb7a2ee904e8b659366a
Kaan Eray <kaan.eray@mni.thm.de>
2023-07-14 18:30:47 +0200

    Added Swagger, MaterialUI nad Websockets

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'frontend', 'src'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
