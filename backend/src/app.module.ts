54842cfb3c0308879c02b655f5ff101e36135a9f
Kaan Eray <kaan.eray@mni.thm.de>
2023-07-14 19:50:33 +0200

    Added TypeOrm and database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,18 @@ import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', '..', 'frontend', 'src'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: './db.sqlite',
+      entities: [],
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
