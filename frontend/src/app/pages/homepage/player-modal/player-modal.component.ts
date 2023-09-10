c06ed929c39041822e8c1c15cb435aedc3e7e437
fynr80 <fatihyanar97@gmail.com>
2023-09-10 16:26:34 +0200

    .

--- frontend/src/app/pages/homepage/player-modal/player-modal.component.ts
+++ frontend/src/app/pages/homepage/player-modal/player-modal.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-player-modal',
   templateUrl: './player-modal.component.html',
-  styleUrls: ['./player-modal.component.css']
+  styleUrls: ['./player-modal.component.css'],
 })
-export class PlayerModalComponent {
-
-}
+export class PlayerModalComponent {}
