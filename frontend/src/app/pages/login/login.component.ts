19361301c199527c6cacf01d53e06e9fda0fa8ab
fynr80 <fatihyanar97@gmail.com>
2023-09-10 13:58:39 +0200

    frontend changes

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent {
   showPassword: boolean = false;
   errorMessage!: string;
   loginbool = false;
-  inputEmail: string | undefined;
+  inputNickname: string | undefined;
   inputPassword: string | undefined;
 
   constructor(public authService: AuthService, private router: Router) {}
@@ -20,9 +20,9 @@ export class LoginComponent {
   // TODO: bug -> if you click on login button and then on register button, the login button is still clicked
   async submit() {
     if (
-      this.inputEmail == undefined ||
+      this.inputNickname == undefined ||
       this.inputPassword == undefined ||
-      this.inputEmail == '' ||
+      this.inputNickname == '' ||
       this.inputPassword == ''
     ) {
       this.showErrorMessage = true;
@@ -31,13 +31,13 @@ export class LoginComponent {
     }
 
     var user = await this.authService
-      .login(this.inputEmail!, this.inputPassword!)
+      .login(this.inputNickname!, this.inputPassword!)
       .catch((err) => {
         console.log(err);
       });
 
     if (user == undefined) {
-      this.errorMessage = 'Falsche E-Mail oder Passwort';
+      this.errorMessage = 'Falscher Nickname oder Passwort';
       this.showErrorMessage = true;
     } else {
       this.router.navigate(['/homepage']);
