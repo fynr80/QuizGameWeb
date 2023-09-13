4cde82d5e4299fabaaaf2c78a87b006e4e265feb
ibrahim <ibrahim.eraslan@thm.de>
2023-09-13 21:51:33 +0200

    quit result implementiert

--- frontend/src/app/pages/homepage/quiz-game/quiz-game.component.ts
+++ frontend/src/app/pages/homepage/quiz-game/quiz-game.component.ts
@@ -20,8 +20,12 @@ export class QuizGameComponent {
   userSubmit: boolean = false;
   userModel: UserModel | undefined;
   counterArr = new Array<boolean>(10);
+  isWin: number = -1;
   showStatistic: boolean = false;
+
   sum: number = 0;
+  enemySum: number = 0;
+
   constructor(
     private authService: AuthService,
     private friendService: FriendService
@@ -32,9 +36,10 @@ export class QuizGameComponent {
     this.getOnSubmitAnswer();
   }
 
-  buttonclicked(answer: string, num: number) {
+  buttonClicked(answer: string, num: number) {
     console.log(this.randomQuestions.length);
     this.userSubmit = true;
+    this.questionNumber++;
     if (this.userModel?.id == this.friendId) {
       this.newid = this.userId;
       this.toogleGameStart = !this.toogleGameStart;
@@ -43,26 +48,50 @@ export class QuizGameComponent {
       this.toogleGameStart = !this.toogleGameStart;
     }
     if (this.friendSubmit == true) {
-      this.questionNumber++;
       this.friendSubmit = false;
       this.userSubmit = false;
     }
     this.counterArr[num] = this.checkValue(answer, num);
-    this.friendService.sendOnSubmitAnswer(this.newid);
-    if (this.questionNumber == 1) {
-      this.showStatistic = true;
-      this.sum = this.pointCalc();
+
+    if (this.questionNumber == 5) {
+      if (this.enemySum == 0) {
+        this.sum = this.pointCalc();
+      } else {
+        this.sum = this.pointCalc();
+        console.log('this.sum');
+
+        console.log(this.sum);
+        console.log('this.enemySum');
+
+        console.log(this.enemySum);
+
+        if (this.enemySum > this.sum) {
+          this.isWin = 2;
+        } else if (this.enemySum < this.sum) {
+          this.isWin = 1;
+        } else {
+          this.isWin = 0;
+        }
+        console.log('this.isWin');
+
+        console.log(this.isWin);
+        this.showStatistic = true;
+      }
     }
+
+    this.friendService.sendOnSubmitAnswer(this.newid, this.sum);
   }
 
   async getOnSubmitAnswer() {
     this.friendService.getOnSubmitAnswer().subscribe((data) => {
-      if (data) {
+      if (data != null) {
         this.friendSubmit = true;
-        this.toogleGameStart = !this.toogleGameStart;
-        if (this.questionNumber == 1) {
+        this.enemySum = data;
+        if (this.questionNumber == 5) {
           this.showStatistic = true;
           this.sum = this.pointCalc();
+        } else {
+          this.toogleGameStart = !this.toogleGameStart;
         }
       }
     });
@@ -85,6 +114,18 @@ export class QuizGameComponent {
         sum++;
       }
     });
+
+    if (this.enemySum > this.sum) {
+      this.isWin = 2;
+    } else if (this.enemySum < this.sum) {
+      this.isWin = 1;
+    } else {
+      this.isWin = 0;
+    }
+
+    if (sum == 0) {
+      sum = -1;
+    }
     return sum;
   }
 }
